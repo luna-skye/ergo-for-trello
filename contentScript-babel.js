1fca69342a54807b62e9a636e0213b4a47310e0f
Andy <sepshuncontact@gmail.com>
2018-04-19 17:16:37 -0400

    Gradient fix & local CSS
    Switched the MinimalDark CSS to work off a local file.
    
    Fixed gradient bug.

--- contentScript-babel.js
+++ contentScript-babel.js
@@ -7,16 +7,15 @@ chrome.storage.sync.get({
 }, (items) => { Object.keys(items).forEach(function (key) { options[key] = items[key]; }); });
 
 window.addEventListener('load', () => {
-	console.log('Trello Ergo is running...');
 	let styles = '';
 
 	// Inject MinimalDark CSS
 	if (options.minimalDark) {
-		let stylesheet = document.createElement('link');
-		stylesheet.rel = "stylesheet";
-		stylesheet.type = "text/css";
-		stylesheet.href = "https://sepshun.com/dev-assets/minimalDark.css";
-		document.body.appendChild(stylesheet);
+		let minimalDarkStyles = document.createElement('link');
+		minimalDarkStyles.rel = "stylesheet";
+		minimalDarkStyles.type = "text/css";
+		minimalDarkStyles.href = chrome.extension.getURL('assets/css/minimalDark.css');
+		document.body.appendChild(minimalDarkStyles);
 
 		// Fix List Headers
 		Array.from(document.getElementsByClassName('list-header-name')).forEach((el) => { el.style.height = '40px'; });
@@ -27,7 +26,11 @@ window.addEventListener('load', () => {
 
 	// Inject Background Gradient CSS
 	if (options.backgroundGradients) {
-		styles += 'body[style*="rgb(0, 121, 191)"] #surface   { background: rgba(0, 121, 191, 1); background: linear-gradient(#0079BF 0%, #1EBDD2 50%, #B5EFA1 100%); } body[style*="rgb(210, 144, 52)"] #surface  { background-color: rgb(210, 144, 52); background: linear-gradient(#D29034 0%, #EB9606 50%, #2A241C 100%); } body[style*="rgb(81, 152, 57)"] #surface   { background: rgba(81, 152, 57, 1); background: linear-gradient(#519839 0%, #A3D21E 50%, #B5EFA1 100%) !important; } body[style*="rgb(176, 70, 50"] #surface    { background-color: rgba(176, 70, 50, 1); background: linear-gradient(#BA311B 0%, #D2881E 75%, #D37319 100%) !important; } body[style*="rgb(137, 96, 158)"] #surface  { background: rgba(84, 97, 192, 1); background: linear-gradient(#5461C0 0%, #801ED2 47%, #963CD3 79%, #972ED8 100%); } body[style*="rgb(205, 90, 145)"] #surface  { background: rgba(234, 83, 125, 1); background: linear-gradient(#EA537D 0%, #D04FD0 50%, #D415FF 100%) !important; } body[style*="rgb(75, 191, 107)"] #surface  { background: rgba(75, 191, 107, 1); background: linear-gradient(#4BBF6B 0%, #4FD0C1 50%, #15ABFF 100%) !important; } body[style*="rgb(0, 174, 204)"] #surface   { background: rgba(0, 174, 204, 1); background: linear-gradient(90deg, #00C0FF, #C301FF) !important; } body[style*="rgb(131, 140, 145)"] #surface { background-color: rgba(131, 140, 145, 1); background: linear-gradient(#838C91, #424242); } ';
+		let gradientStyles = document.createElement('link');
+		gradientStyles.rel = "stylesheet";
+		gradientStyles.type = "text/css";
+		gradientStyles.href = chrome.extension.getURL('assets/css/gradients.css');
+		document.body.appendChild(gradientStyles);
 	}
 
 	// Card Counting
@@ -47,26 +50,34 @@ window.addEventListener('load', () => {
 				totalCardCount.classList.add('board-header-btn');
 				totalCardCount.appendChild(document.createTextNode('0 total cards'));
 
-				document.querySelector('.board-header-btns.mod-left').appendChild(divider);
-				document.querySelector('.board-header-btns.mod-left').appendChild(totalCardCount);
+				let container = document.querySelector('.board-header-btns.mod-left');
+				if (container) {
+					container.appendChild(divider);
+					container.appendChild(totalCardCount);
+				}
+
 			}
 
-			Array.from(document.querySelectorAll('.list')).forEach((list) => {
-				if (!list.children[0].children[4].children[0].classList.contains('card-count')) {
-					let countEl = document.createElement('div');
-					countEl.classList.add('card-count');
-					countEl.appendChild(document.createTextNode('0'));
-					list.children[0].children[4].prepend(countEl);
-				};
 
-				let count = 0;
-				Array.from(list.children[1].children).forEach((card) => {
-					if (!card.classList.contains('card-composer')) { count++; totalCount++; }
+			let list = document.querySelectorAll('.list');
+			if (list) {
+				Array.from(list).forEach((list) => {
+					if (!list.children[0].children[4].children[0].classList.contains('card-count')) {
+						let countEl = document.createElement('div');
+						countEl.classList.add('card-count');
+						countEl.appendChild(document.createTextNode('0'));
+						list.children[0].children[4].prepend(countEl);
+					};
+
+					let count = 0;
+					Array.from(list.children[1].children).forEach((card) => {
+						if (!card.classList.contains('card-composer')) { count++; totalCount++; }
+					});
+					list.children[0].children[4].children[0].innerText = count;
 				});
-				list.children[0].children[4].children[0].innerText = count;
-			});
 
-			document.getElementById('total-card-count').innerText = totalCount + ' total cards';
+				document.getElementById('total-card-count').innerText = totalCount + ' total cards';
+			}
 		}
 
 		// Update The Counters
--- contentScript.js
+++ contentScript.js
@@ -13,16 +13,15 @@ chrome.storage.sync.get({
 });
 
 window.addEventListener('load', function () {
-	console.log('Trello Ergo is running...');
 	var styles = '';
 
 	// Inject MinimalDark CSS
 	if (options.minimalDark) {
-		var stylesheet = document.createElement('link');
-		stylesheet.rel = "stylesheet";
-		stylesheet.type = "text/css";
-		stylesheet.href = "https://sepshun.com/dev-assets/minimalDark.css";
-		document.body.appendChild(stylesheet);
+		var minimalDarkStyles = document.createElement('link');
+		minimalDarkStyles.rel = "stylesheet";
+		minimalDarkStyles.type = "text/css";
+		minimalDarkStyles.href = chrome.extension.getURL('assets/css/minimalDark.css');
+		document.body.appendChild(minimalDarkStyles);
 
 		// Fix List Headers
 		Array.from(document.getElementsByClassName('list-header-name')).forEach(function (el) {
@@ -37,7 +36,11 @@ window.addEventListener('load', function () {
 
 	// Inject Background Gradient CSS
 	if (options.backgroundGradients) {
-		styles += '#classic-body[style*="rgb(0, 121, 191)"] { background: rgba(0, 121, 191, 1); background: linear-gradient(#0079BF 0%, #1EBDD2 50%, #B5EFA1 100%); } #classic-body[style*="rgb(210, 144, 52)"]{ background-color: rgb(210, 144, 52); background: linear-gradient(#D29034 0%, #EB9606 50%, #2A241C 100%); } #classic-body[style*="rgb(81, 152, 57)"] { background: rgba(81, 152, 57, 1); background: linear-gradient(#519839 0%, #A3D21E 50%, #B5EFA1 100%) !important; } #classic-body[style*="rgb(176, 70, 50"]  { background-color: rgba(176, 70, 50, 1); background: linear-gradient(#BA311B 0%, #D2881E 75%, #D37319 100%) !important; } #classic-body[style*="rgb(137, 96, 158)"]{ background: rgba(84, 97, 192, 1); background: linear-gradient(#5461C0 0%, #801ED2 47%, #963CD3 79%, #972ED8 100%); } #classic-body[style*="rgb(205, 90, 145)"]{ background: rgba(234, 83, 125, 1); background: linear-gradient(#EA537D 0%, #D04FD0 50%, #D415FF 100%) !important; } #classic-body[style*="rgb(75, 191, 107)"]{ background: rgba(75, 191, 107, 1); background: linear-gradient(#4BBF6B 0%, #4FD0C1 50%, #15ABFF 100%) !important; } #classic-body[style*="rgb(0, 174, 204)"] { background: rgba(0, 174, 204, 1); background: linear-gradient(90deg, #00C0FF, #C301FF) !important; } #classic-body[style*="rgb(131, 140, 145)"] background-color: rgba(131, 140, 145, 1); background: linear-gradient(#838C91, #424242); } ';
+		var gradientStyles = document.createElement('link');
+		gradientStyles.rel = "stylesheet";
+		gradientStyles.type = "text/css";
+		gradientStyles.href = chrome.extension.getURL('assets/css/gradients.css');
+		document.body.appendChild(gradientStyles);
 	}
 
 	// Card Counting
@@ -57,28 +60,34 @@ window.addEventListener('load', function () {
 				totalCardCount.classList.add('board-header-btn');
 				totalCardCount.appendChild(document.createTextNode('0 total cards'));
 
-				document.querySelector('.board-header-btns.mod-left').appendChild(divider);
-				document.querySelector('.board-header-btns.mod-left').appendChild(totalCardCount);
+				var container = document.querySelector('.board-header-btns.mod-left');
+				if (container) {
+					container.appendChild(divider);
+					container.appendChild(totalCardCount);
+				}
 			}
 
-			Array.from(document.querySelectorAll('.list')).forEach(function (list) {
-				if (!list.children[0].children[4].children[0].classList.contains('card-count')) {
-					var countEl = document.createElement('div');
-					countEl.classList.add('card-count');
-					countEl.appendChild(document.createTextNode('0'));
-					list.children[0].children[4].prepend(countEl);
-				};
+			var list = document.querySelectorAll('.list');
+			if (list) {
+				Array.from(list).forEach(function (list) {
+					if (!list.children[0].children[4].children[0].classList.contains('card-count')) {
+						var countEl = document.createElement('div');
+						countEl.classList.add('card-count');
+						countEl.appendChild(document.createTextNode('0'));
+						list.children[0].children[4].prepend(countEl);
+					};
 
-				var count = 0;
-				Array.from(list.children[1].children).forEach(function (card) {
-					if (!card.classList.contains('card-composer')) {
-						count++;totalCount++;
-					}
+					var count = 0;
+					Array.from(list.children[1].children).forEach(function (card) {
+						if (!card.classList.contains('card-composer')) {
+							count++;totalCount++;
+						}
+					});
+					list.children[0].children[4].children[0].innerText = count;
 				});
-				list.children[0].children[4].children[0].innerText = count;
-			});
 
-			document.getElementById('total-card-count').innerText = totalCount + ' total cards';
+				document.getElementById('total-card-count').innerText = totalCount + ' total cards';
+			}
 		};
 
 		// Update The Counters
@@ -124,9 +133,9 @@ window.addEventListener('load', function () {
 
 	// Inject Styles
 	if (styles != '') {
-		var _stylesheet = document.createElement('style');
-		_stylesheet.type = 'text/css';
-		_stylesheet.appendChild(document.createTextNode(styles));
-		document.body.appendChild(_stylesheet);
+		var stylesheet = document.createElement('style');
+		stylesheet.type = 'text/css';
+		stylesheet.appendChild(document.createTextNode(styles));
+		document.body.appendChild(stylesheet);
 	}
 });
